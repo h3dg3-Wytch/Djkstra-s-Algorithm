7786197e91ef6ec3af81d59e81be4d1cd054f3a5
Rex <rexpeters3n@gmail.com>
2016-03-23 11:20:17 -0400

    feat Add ability to find file by path

--- Test.java
+++ Test.java
@@ -8,47 +8,78 @@ import java.util.List;
 public class Test {
 
     public static void main(String[] args) {
-
-
-        //If the length is zero, then the user did not input an input file
-        if (args.length == 0) {
-            try {
-                List<Node> nodes = IO.readFromFile("25000.txt");
-                
-                Dijkstra dijkstra = new Dijkstra(nodes);
-                dijkstra.execute();
-
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            //There are arguments, if they inlcluded the number one in there work, then it is a microsoft windows path
-        } else if (!args[1].equals("1")) {
-            String path = args[0];
-            try {
-                List<Node> nodes = IO.readFromFile(path);
-                System.out.println(nodes.get(0));
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        } else {
-            String path = args[0];
-            path = path.replace("\\", "/");
-            try {
-                List<Node> nodes = IO.readFromFile(path);
-            } catch (FileNotFoundException e) {
+        //If the length of args is zero, we return, we need valid input
+        if(args.length == 0){
+            System.out.println("Please enter a valid file name that is currently in your directory or a valid file path!");
+        }else{
+            //if the user enters one in the command prompt , they are on windows and need special formating for the path
+            if(args[1] == "1") {
+                String path = args[0];
+                path = path.replace("\\", "/");
+                try {
+                    List<Node> nodes = IO.readFromFile(path);
+                }catch (FileNotFoundException e) {
 
                 System.out.println("Error: file not found!");
 
-            } catch (IOException e) {
-
+                }catch (IOException e) {
                 System.out.println("Error: IOException, something went wrong!");
+                }
 
+            }else{
+                String input = args[0];
+                try {
+                    List<Node> nodeList = IO.readFromFile(input);
+                    for (Node n : nodeList) {
+                        System.out.println(n);
+                    }
+                } catch (Exception e) {
+                    System.out.println("There was an error! Please check your input and try again");
+                }
             }
-
         }
-
-
     }
+
+//
+//        //If the length is zero, then the user did not input an input file
+//        if (args.length == 0) {
+//            try {
+//                List<Node> nodes = IO.readFromFile("25000.txt");
+//
+//                Dijkstra dijkstra = new Dijkstra(nodes);
+//                dijkstra.execute();
+//
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//            }
+//            //There are arguments, if they inlcluded the number one in there work, then it is a microsoft windows path
+//        } else if (!args[1].equals("1")) {
+//            String path = args[0];
+//            try {
+//                List<Node> nodes = IO.readFromFile(path);
+//                System.out.println(nodes.get(0));
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//            }
+//        } else {
+//            String path = args[0];
+//            path = path.replace("\\", "/");
+//            try {
+//                List<Node> nodes = IO.readFromFile(path);
+//            } catch (FileNotFoundException e) {
+//
+//                System.out.println("Error: file not found!");
+//
+//            } catch (IOException e) {
+//
+//                System.out.println("Error: IOException, something went wrong!");
+//
+//            }
+//
+//        }
+//
+//
+//    }
 }
 
 
